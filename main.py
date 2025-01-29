925d0e71523f2ce91114e8a09e554f2b97a2f038
domi <szdomi0823@proton.me>
2025-01-30 00:01:51 +0100

    v0.1

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from netaddr import *
+from ipaddress import *
+
+#Inputs
+ip = ip_address(input('Ip: '))
+mask = input('Mask(CIDR): ')
+subnets = int(input('Subnets: '))
+
+#Subnets list
+n1 = list((ip_network(str(ip) + str(mask)).subnets(prefixlen_diff=subnets)))
+print(n1[0]) #Get the first element of the 'n1' list
 
 
-ip = IPNetwork('192.168.11.50/24')
-subnets = int(input('subnets: '))
-
-subnets
-
+#Default gateway
+atjaro = str(ip) + 1
+print(atjaro)
 
 
-print(ip.netmask)
-print(ip.network)
-print(ip.broadcast)
-print(ip.size)
+
 
 
 
