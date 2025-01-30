6bdfe79462d545eefa94623c4ca3cbe4cb796418
Hidi <87263099+TheH1D1@users.noreply.github.com>
2025-01-30 13:04:29 +0100

    Add files via upload
    v0.2

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ from ipaddress import *
 ip = ip_address(input('Ip: '))
 mask = input('Mask(CIDR): ')
 subnets = int(input('Subnets: '))
+#pcs = int(input('PCS: '))
 
 #Subnets list
 n1 = list((ip_network(str(ip) + str(mask)).subnets(prefixlen_diff=subnets)))
-print(n1[0]) #Get the first element of the 'n1' list
+print('Network address: ',n1[0]) #Get the first element of the 'n1' list
+print('Subnets: ', n1)
+
+#Usable hosts
+hosts = list(ip_network(ip).hosts())
+print('hosts: ',hosts)
+
+
 
 
 #Default gateway
-atjaro = str(ip) + 1
-print(atjaro)
-
-
-
+def default_gateway():
+    atjaro = ip + int(1)
+    print('Default gateway: ',atjaro)
+default_gateway()
 
 
 
